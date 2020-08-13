dcb49c074fa43f27510a95fe16e0372109bccb4d
TimMatsumoto <tmatsumoto1997@gmail.com>
2020-08-13 14:55:50 -0700

    added quadratically regularized OT

--- src/OptimalTransport.jl
+++ src/OptimalTransport.jl
@@ -1,12 +1,14 @@
 # OptimalTransport.jl -- optimal transportation algorithms for Julia
 # Author: Stephen Zhang (syz@math.ubc.ca)
-# Contributors: David Widmann (@devmotion)
+# Contributors: David Widmann (@devmotion) Tetsuya Matsumoto (tmatsumoto1997@gmail.com)
 
 module OptimalTransport
 
 using PyCall
 using Distances
 using LinearAlgebra
+using IterativeSolvers
+using SparseArrays
 
 export sinkhorn, sinkhorn2, pot_sinkhorn, pot_sinkhorn2
 export emd, emd2
@@ -332,5 +334,136 @@ function sinkhorn_stabilized(mu, nu, C, eps; absorb_tol = 1e3, max_iter = 1000, 
     end
     return getK(C, alpha, beta, eps, mu, nu)
 end
+
+
+
+"""
+SemiSmoothNewton(mu, nu, C, ϵ; kwargs...)
+
+Computes the optimal transport map of histograms `mu` and `nu` with cost matrix `C` and quadratic regularization parameter `ϵ`, 
+using the semismooth Newton algorithm [Lorenz 2016].
+
+This implementation makes use of IterativeSolvers.jl and SparseArrays.jl.
+
+Parameters:
+θ: starting Armijo parameter.
+tol: tolerance of marginal error.
+maxiter: maximum interation number.
+κ: control parameter of Armijo.
+δ: small constant for the numerical stability of conjugate gradient iterative solver.
+
+Tips:
+If the algorithm does not converge, try some different values of θ.
+
+Reference:
+Lorenz, D.A., Manns, P. and Meyer, C., 2019. Quadratically regularized optimal transport. arXiv preprint arXiv:1903.01112v4.
+"""
+function SemiSmoothNewton(mu, nu, C, ϵ;        
+    θ = 0.01,
+    tol = 0.01,
+    maxiter = 500,
+    κ = 0.5,
+    δ = 1e-5
+    )
+        
+    
+
+    if !(sum(mu) ≈ sum(nu))
+        throw(ArgumentError("Error: mu and nu must lie in the simplex"))
+    end
+
+    N = length(mu)
+    M = length(nu)
+
+    # initialize dual potentials as uniforms
+    a = ones(M)./M
+    b = ones(N)./N
+
+    γ = spzeros(M, N)
+
+    converged = false
+
+    function DualObjective(a, b)
+        A = a.*ones(N)' + ones(M).*b' - C
+
+        return 0.5 * norm(A[A.>0], 2) - ϵ*(dot(nu, a)+ dot(mu, b))
+    end
+    
+    # computes minimizing directions
+    function search_dir(a, b)
+        
+        P  = a*ones(N)' .+ ones(M)*b' .- C
+        
+        σ = 1.0*sparse(P .>= 0)
+        γ = sparse(max.(P, 0)./ ϵ)
+        
+        G = vcat(
+            hcat(spdiagm(0 => σ*ones(N)), σ), 
+            hcat(sparse(σ'), 
+            spdiagm(0 => sparse(σ')*ones(M) ))
+            )
+        
+        h = vcat(
+            γ*ones(N) - nu, 
+            sparse(γ')*ones(M) - mu
+            )
+                    
+        x = cg(G + δ*I, -ϵ * h)
+        
+        return x[1:M], x[M+1:end]
+    end
+
+    # computes optimal maginitude in the minimizing directions
+    function search_t(a, b, da, db, θ)
+        
+        d =  ϵ * dot(γ , (da .* ones(N)' .+ ones(M) .* db')) -ϵ*(dot(da, nu) + dot(db, mu))
+        
+        ϕ₀ = DualObjective(a, b)
+        t = 1
+        
+        while DualObjective(a+t*da, b+t*db) >= ϕ₀ + t*θ*d
+            t *= κ
+            
+            if t < 1e-15
+                # @warn "@ i = $i, t = $t , armijo did not converge"
+                break
+            end
+            
+        end
+        return t
+    end
+
+    for i in 1:maxiter
+        
+        da, db = search_dir(a, b)
+        
+        t = search_t(a, b, da, db, θ)
+        
+        a += t*da
+        b += t*db
+        
+        err1 = norm(γ*ones(N) -nu, Inf) 
+        err2 = norm(sparse(γ')*ones(M) - mu, Inf)
+        
+        if err1 <= tol && err2 <= tol
+            converged = true
+            print("Converged @ i = $i with marginal errors: \n
+            err1 = $err1, err2 = $err2 \n")
+            break
+        elseif i == maxiter
+            print("Not Converged with errors:\n err1 = $err1, err2 = $err2 \n")
+        end
+        
+        
+    end
+
+    if  !converged
+        @warn "SemiSmooth Newton algorithm did not converge"    
+    end
+
+    return Matrix(γ')
+end
+
+
 
 end
