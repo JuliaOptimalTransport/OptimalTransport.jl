69a307a756cab9a8bf2ce7ae5d4237da5bf32171
David Widmann <dev+git@devmotion.de>
2020-12-12 04:21:50 +0100

    Fix LP formulation and solver

--- src/SimplexOT.jl
+++ src/SimplexOT.jl
@@ -1,82 +1,82 @@
-using MathOptInterface
-
-const MOI = MathOptInterface
 """
-ToSimplexFormat(`\\mu,\\nu,C`)
-Transforms the matrix format of the Discrete Optimal Transport
-problem to the Linear Programming Format.
+    toSimplexFormat(μ, ν, C)
 
-Variables `\\mu,\\nu` are the probability distributions.
+Formulate the discrete optimal transport problem with source `μ`, target `ν`, and
+cost matrix `C` as a linear programming problem.
 
+More concretely, this function returns a tuple of the variables `c`, `A`, and `b` of the
+LP problem
 ```math
-{\\bf A} = \\begin{bmatrix}
-{\\bf 1_n}^T \\otimes \\mathbb I_m\\
-\\mathbb I_n \\otimes {\\bf 1_m}^T
-\\end{bmatrix} \\
-b = [\\mu,\\nu]^T \\
-c = [C_{1,1},C_{2,1},...,C_{n,1},C_{2,1},...,C_{n,m}]^T \\
-p = [P_{1,1},P_{2,1},...,P_{n,1},P_{2,1},...,P_{n,m}]^T \\
+\\begin{aligned}
+\\min_{p} c^T p & \\\\
+\\text{subject to } Ap &= b \\\\
+0 &\\leq p
+\\end{aligned}
 ```
-
-The LP problem for OT is
+where
 ```math
-min c^T p \\
-Ap = b \\
-p \\geq 0\\
+\\begin{aligned}
+p &= [P_{1,1},P_{2,1},\\ldots,P_{n,1},P_{2,1},\\ldots,P_{n,m}]^T, \\\\
+c &= [C_{1,1},C_{2,1},\\ldots,C_{n,1},C_{2,1},\\ldots,C_{n,m}]^T, \\\\
+A &= \\begin{bmatrix}
+1_n^T \\otimes I_m \\\\
+I_n \\otimes 1_m^T
+\\end{bmatrix}, \\\\
+b &= [μ, ν]^T.
+\\end{aligned}
 ```
 """
-function ToSimplexFormat(μ,ν,C)
+function toSimplexFormat(μ, ν, C)
+    m = length(μ)
+    n = length(ν)
 
-	n,m = size(μ)[1],size(ν)[1]
-	# If n=/= m, then fill with zeros until they are equal
-	if size(μ)[1]!=size(ν)[1]
-		if n - m > 0
-		    d  = n-m
-            C  = vcat(C,zeros(n,d))
-		    ν  = vcat(ν,zeros(d));
-		elseif n - m < 0
-		    d = m-n
-			C = hcat(C,zeros(m,d))
-		    μ = vcat(μ,zeros(d))
-		end
-	end
-	n,m = max(n,m),max(n,m); # They are now equal
+    c = vec(C)
+    b = Vcat(μ, ν)
+    A = Vcat(
+        Kron(Ones(1, n), Eye(m)),
+        Kron(Eye(n), Ones(1, m)),
+    )
 
-	c   = reshape(C,n*m,1)[:] # Matrix to vector format
-	b   = vcat(μ,ν)           # Distributions constraints
-	A = [kron(ones(n)',1.0*Matrix(I,m,m))
-     	 kron(1.0*Matrix(I,n,n),ones(m)')]
-
-   	return c,A,b,μ,ν
+    return c, A, b
 end
 
-
 """
-SolveLP(c,A,b)
-Solve the following Linear Programming problem.
-LP problem format:
-       min  dot(c, x)
-subject to A x = b
-             0 ≤ x
+    solveLP(c, A, b, optimizer)
 
-'model' consists in the optimizer of choice by the user.
-e.g: model = Tulip.Optimizer()
+Solve the linear programming problem
+```math
+\\begin{aligned}
+\\min_{x} c^T x & \\\\
+\\text{subject to } Ax &= b \\\\
+0 &\\leq x
+\\end{aligned}
+```
+with the given `optimizer`.
+
+Possible choices of `optimizer` are `Tulip.Optimizer()` or `Clp.Optimizer()` in the
+`Tulip` and `Clp` packages, respectively.
 """
-function SolveLP(c,A,b,model::MOI.ModelLike)
-	x = MOI.add_variables(model, length(c));
-	MOI.set(model, MOI.ObjectiveFunction{MOI.ScalarAffineFunction{Float64}}(),
-	            MOI.ScalarAffineFunction(MOI.ScalarAffineTerm.(c, x), 0.0))
-	MOI.set(model, MOI.ObjectiveSense(), MOI.MIN_SENSE)
-	for xi in x
-	    MOI.add_constraint(model, MOI.SingleVariable(xi), MOI.GreaterThan(0.0))
-	end
+function solveLP(c, A, b, model::MOI.ModelLike)
+    x = MOI.add_variables(model, length(c))
+    MOI.set(
+        model,
+        MOI.ObjectiveFunction{MOI.ScalarAffineFunction{eltype(c)}}(),
+        MOI.ScalarAffineFunction(MOI.ScalarAffineTerm.(c, x), zero(eltype(c))),
+    )
+    MOI.set(model, MOI.ObjectiveSense(), MOI.MIN_SENSE)
+    for xi in x
+        MOI.add_constraint(model, MOI.SingleVariable(xi), MOI.GreaterThan(0.0))
+    end
 
-	for (i,row) in enumerate(eachrow(A))
-	    row_function = MOI.ScalarAffineFunction(MOI.ScalarAffineTerm.(row, x), 0.0);
-	    MOI.add_constraint(model, row_function, MOI.EqualTo(b[i]))
-	end
-	MOI.optimize!(model)
-	p = MOI.get(model, MOI.VariablePrimal(), x);
-	return p
+    for (row, bi) in zip(eachrow(A), b)
+        row_function = MOI.ScalarAffineFunction(
+            MOI.ScalarAffineTerm.(row, x), zero(eltype(row)),
+        )
+        MOI.add_constraint(model, row_function, MOI.EqualTo(bi))
+    end
+
+    MOI.optimize!(model)
+    p = MOI.get(model, MOI.VariablePrimal(), x)
+
+    return p
 end
-
