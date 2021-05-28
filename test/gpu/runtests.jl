3011bc797afa8174f8eff9b0fa39088aae542d32
David Widmann <david.widmann@it.uu.se>
2021-05-28 17:03:55 +0200

    Add tests on the GPU

---
+++ test/gpu/runtests.jl
@@ -0,0 +1,15 @@
+using OptimalTransport
+
+using CUDA
+using Distances
+
+using Test
+
+@testset "gpu" begin
+    # ensure that a GPU is available
+    if CUDA.functional()
+        @testset "Simple GPU" begin include("simple_gpu.jl") end
+    else
+        @warn "skipped GPU tests: no GPU available"
+    end
+end
