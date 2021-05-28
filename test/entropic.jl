66bfcb544de0e66d9b58c87fe0d73d290ac055dd
David Widmann <david.widmann@it.uu.se>
2021-05-29 00:19:01 +0200

    Change test structure

---
+++ test/entropic.jl
@@ -0,0 +1,146 @@
+using OptimalTransport
+
+using Distances
+using PythonOT: PythonOT
+
+using Random
+using Test
+
+const POT = PythonOT
+
+Random.seed!(100)
+
+@testset "entropic.jl" begin
+    @testset "sinkhorn" begin
+        M = 250
+        N = 200
+
+        @testset "example" begin
+            # create two uniform histograms
+            μ = fill(1 / M, M)
+            ν = fill(1 / N, N)
+
+            # create random cost matrix
+            C = pairwise(SqEuclidean(), rand(1, M), rand(1, N); dims=2)
+
+            # compute optimal transport map (Julia implementation + POT)
+            eps = 0.01
+            γ = sinkhorn(μ, ν, C, eps; maxiter=5_000, rtol=1e-9)
+            γ_pot = POT.sinkhorn(μ, ν, C, eps; numItermax=5_000, stopThr=1e-9)
+            @test γ_pot ≈ γ rtol = 1e-6
+
+            # compute optimal transport cost
+            c = sinkhorn2(μ, ν, C, eps; maxiter=5_000, rtol=1e-9)
+
+            # with regularization term
+            c_w_regularization = sinkhorn2(μ, ν, C, eps; maxiter=5_000, regularization=true)
+            @test c_w_regularization ≈ c + eps * sum(x -> iszero(x) ? x : x * log(x), γ)
+
+            # compare with POT
+            c_pot = POT.sinkhorn2(μ, ν, C, eps; numItermax=5_000, stopThr=1e-9)[1]
+            @test c_pot ≈ c
+
+            # ensure that provided map is used and correct
+            c2 = sinkhorn2(similar(μ), similar(ν), C, rand(); plan=γ)
+            @test c2 ≈ c
+            c2_w_regularization = sinkhorn2(
+                similar(μ), similar(ν), C, eps; plan=γ, regularization=true
+            )
+            @test c2_w_regularization ≈ c_w_regularization
+        end
+
+        # different element type
+        @testset "Float32" begin
+            # create two uniform histograms
+            μ = fill(Float32(1 / M), M)
+            ν = fill(Float32(1 / N), N)
+
+            # create random cost matrix
+            C = pairwise(SqEuclidean(), rand(Float32, 1, M), rand(Float32, 1, N); dims=2)
+
+            # compute optimal transport map (Julia implementation + POT)
+            eps = 0.01f0
+            γ = sinkhorn(μ, ν, C, eps; maxiter=5_000, rtol=1e-6)
+            @test eltype(γ) === Float32
+
+            γ_pot = POT.sinkhorn(μ, ν, C, eps; numItermax=5_000, stopThr=1e-6)
+            @test Float32.(γ_pot) ≈ γ rtol = 1e-3
+
+            # compute optimal transport cost
+            c = sinkhorn2(μ, ν, C, eps; maxiter=5_000, rtol=1e-6)
+            @test c isa Float32
+
+            # with regularization term
+            c_w_regularization = sinkhorn2(
+                μ, ν, C, eps; maxiter=5_000, rtol=1e-6, regularization=true
+            )
+            @test c_w_regularization ≈ c + eps * sum(x -> iszero(x) ? x : x * log(x), γ)
+
+            # compare with POT
+            c_pot = POT.sinkhorn2(μ, ν, C, eps; numItermax=5_000, stopThr=1e-6)[1]
+            @test Float32(c_pot) ≈ c rtol = 1e-3
+        end
+
+        @testset "deprecations" begin
+            # create two uniform histograms
+            μ = fill(1 / M, M)
+            ν = fill(1 / N, N)
+
+            # create random cost matrix
+            C = pairwise(SqEuclidean(), rand(1, M), rand(1, N); dims=2)
+
+            # check `sinkhorn2`
+            eps = 0.01
+            c = sinkhorn2(μ, ν, C, eps; atol=1e-6)
+            @test (@test_deprecated sinkhorn2(μ, ν, C, eps; tol=1e-6)) == c
+            c = sinkhorn2(μ, ν, C, eps; check_convergence=5)
+            @test (@test_deprecated sinkhorn2(μ, ν, C, eps; check_marginal_step=5)) == c
+
+            # check `sinkhorn_gibbs
+            K = @. exp(-C / eps)
+            γ = OptimalTransport.sinkhorn_gibbs(μ, ν, K; atol=1e-6)
+            @test (@test_deprecated OptimalTransport.sinkhorn_gibbs(μ, ν, K; tol=1e-6)) == γ
+            γ = OptimalTransport.sinkhorn_gibbs(μ, ν, K; check_convergence=5)
+            @test (@test_deprecated OptimalTransport.sinkhorn_gibbs(
+                μ, ν, K; check_marginal_step=5
+            )) == γ
+        end
+    end
+
+    @testset "sinkhorn_stabilized" begin
+        M = 250
+        N = 200
+
+        # create two uniform histograms
+        μ = fill(1 / M, M)
+        ν = fill(1 / N, N)
+
+        # create random cost matrix
+        C = pairwise(SqEuclidean(), rand(1, M), rand(1, N); dims=2)
+
+        # compute optimal transport map (Julia implementation + POT)
+        eps = 0.01
+        γ = sinkhorn_stabilized(μ, ν, C, eps)
+        γ_pot = POT.sinkhorn(μ, ν, C, eps; method="sinkhorn_stabilized")
+        @test γ ≈ γ_pot rtol=1e-6
+    end
+
+    @testset "sinkhorn_barycenter" begin
+        # set up support
+        support = range(-1; stop=1, length=250)
+        μ1 = exp.(-(support .+ 0.5) .^ 2 ./ 0.1^2)
+        μ1 ./= sum(μ1)
+        μ2 = exp.(-(support .- 0.5) .^ 2 ./ 0.1^2)
+        μ2 ./= sum(μ2)
+        μ_all = hcat(μ1, μ2)'
+
+        # create cost matrix
+        C = pairwise(SqEuclidean(), support'; dims=2)
+
+        # compute Sinkhorn barycenter (Julia implementation + POT)
+        eps = 0.01
+        μ_interp = sinkhorn_barycenter(μ_all, [C, C], eps, [0.5, 0.5])
+        μ_interp_pot = POT.barycenter(μ_all', C, eps; weights=[0.5, 0.5], stopThr=1e-9)
+        @test μ_interp ≈ μ_interp_pot
+    end
+end
--- test/gpu/simple_gpu.jl
+++ test/gpu/simple_gpu.jl
@@ -1,51 +1,66 @@
+using OptimalTransport
+
+using CUDA
+using Distances
+
+using Random
+using Test
+
+Random.seed!(100)
+
 @testset "simple_gpu.jl" begin
-    @testset "sinkhorn" begin
-        # source histogram
-        m = 200
-        μ = rand(Float32, m)
-        μ ./= sum(μ)
+    # ensure that a GPU is available
+    if !CUDA.functional()
+        @warn "skipped GPU tests: no GPU available"
+    else
+        @testset "sinkhorn" begin
+            # source histogram
+            m = 200
+            μ = rand(Float32, m)
+            μ ./= sum(μ)
 
-        # target histogram
-        n = 250
-        ν = rand(Float32, n)
-        ν ./= sum(ν)
+            # target histogram
+            n = 250
+            ν = rand(Float32, n)
+            ν ./= sum(ν)
 
-        # random cost matrix
-        C = pairwise(SqEuclidean(), randn(Float32, 1, m), randn(Float32, 1, n); dims=2)
+            # random cost matrix
+            C = pairwise(SqEuclidean(), randn(Float32, 1, m), randn(Float32, 1, n); dims=2)
 
-        # compute transport plan and cost on the GPU
-        ε = 0.01f0
-        γ = sinkhorn(cu(μ), cu(ν), cu(C), ε)
-        c = sinkhorn2(cu(μ), cu(ν), cu(C), ε)
+            # compute transport plan and cost on the GPU
+            ε = 0.01f0
+            γ = sinkhorn(cu(μ), cu(ν), cu(C), ε)
+            c = sinkhorn2(cu(μ), cu(ν), cu(C), ε)
 
-        # compare with results on the CPU
-        @test γ ≈ sinkhorn(μ, ν, C, ε)
-        @test c ≈ sinkhorn2(μ, ν, C, ε)
-    end
+            # compare with results on the CPU
+            @test γ ≈ sinkhorn(μ, ν, C, ε)
+            @test c ≈ sinkhorn2(μ, ν, C, ε)
+        end
 
-    @testset "sinkhorn_unbalanced" begin
-        # source histogram
-        m = 200
-        μ = rand(Float32, m)
-        μ ./= 1.5f0 * sum(μ)
+        @testset "sinkhorn_unbalanced" begin
+            # source histogram
+            m = 200
+            μ = rand(Float32, m)
+            μ ./= 1.5f0 * sum(μ)
 
-        # target histogram
-        n = 250
-        ν = rand(Float32, n)
-        ν ./= sum(ν)
+            # target histogram
+            n = 250
+            ν = rand(Float32, n)
+            ν ./= sum(ν)
 
-        # random cost matrix
-        C = pairwise(SqEuclidean(), randn(Float32, 1, m), randn(Float32, 1, n); dims=2)
+            # random cost matrix
+            C = pairwise(SqEuclidean(), randn(Float32, 1, m), randn(Float32, 1, n); dims=2)
 
-        # compute transport plan and cost on the GPU
-        ε = 0.01f0
-        λ1 = 0.4f0
-        λ2 = 0.6f0
-        γ = sinkhorn_unbalanced(cu(μ), cu(ν), cu(C), λ1, λ2, ε)
-        c = sinkhorn_unbalanced2(cu(μ), cu(ν), cu(C), λ1, λ2, ε)
+            # compute transport plan and cost on the GPU
+            ε = 0.01f0
+            λ1 = 0.4f0
+            λ2 = 0.6f0
+            γ = sinkhorn_unbalanced(cu(μ), cu(ν), cu(C), λ1, λ2, ε)
+            c = sinkhorn_unbalanced2(cu(μ), cu(ν), cu(C), λ1, λ2, ε)
 
-        # compare with results on the CPU
-        @test γ ≈ sinkhorn_unbalanced(μ, ν, C, λ1, λ2, ε)
-        @test c ≈ sinkhorn_unbalanced2(μ, ν, C, λ1, λ2, ε)
+            # compare with results on the CPU
+            @test γ ≈ sinkhorn_unbalanced(μ, ν, C, λ1, λ2, ε)
+            @test c ≈ sinkhorn_unbalanced2(μ, ν, C, λ1, λ2, ε)
+        end
     end
 end
