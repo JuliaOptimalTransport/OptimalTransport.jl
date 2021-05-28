c9e1c87cb44a3c6ef0018e8fbfbdf6c06d49a419
David Widmann <devmotion@users.noreply.github.com>
2021-05-28 19:45:11 +0200

    Fix format
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- test/gpu/runtests.jl
+++ test/gpu/runtests.jl
@@ -8,7 +8,9 @@ using Test
 @testset "gpu" begin
     # ensure that a GPU is available
     if CUDA.functional()
-        @testset "Simple GPU" begin include("simple_gpu.jl") end
+        @testset "Simple GPU" begin
+            include("simple_gpu.jl")
+        end
     else
         @warn "skipped GPU tests: no GPU available"
     end
