d4e6510c53b3a610518f8bbd525b79d3e5c5726b
David Widmann <devmotion@users.noreply.github.com>
2021-05-29 08:19:56 +0200

    Add tests with CUDA (#83)
    * Add tests on the GPU
    
    * Add Pkg to dependencies
    
    * Add PYTHON environment variable
    
    * Add missing bracket
    
    * Fix format
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * Fix tests
    
    * Reorganize tests
    
    * Fix buildkite pipeline
    
    * Change test structure
    
    * Update GPU tests
    
    * Fix format
    
    * Fix dependencies
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

---
+++ test/quadratic.jl
@@ -0,0 +1,33 @@
+using OptimalTransport
+
+using Distances
+using PythonOT: PythonOT
+
+using Random
+using Test
+
+const POT = PythonOT
+
+Random.seed!(100)
+
+@testset "quadratic.jl" begin
+    @testset "quadreg" begin
+        M = 250
+        N = 200
+
+        # create two uniform histograms
+        μ = fill(1 / M, M)
+        ν = fill(1 / N, N)
+
+        # create random cost matrix
+        C = pairwise(SqEuclidean(), rand(1, M), rand(1, N); dims=2)
+
+        # compute optimal transport map (Julia implementation + POT)
+        eps = 0.25
+        γ = quadreg(μ, ν, C, eps)
+        γ_pot = POT.Smooth.smooth_ot_dual(μ, ν, C, eps; stopThr=1e-9)
+
+        # need to use a larger tolerance here because of a quirk with the POT solver
+        @test γ ≈ γ_pot rtol = 1e-1
+    end
+end
