e040b8d07ca6b0069b6036a6ce6348174f5f06bb
David Widmann <david.widmann@it.uu.se>
2021-06-02 01:25:49 +0200

    Allow `Real`-valued orders as well

--- test/wasserstein.jl
+++ test/wasserstein.jl
@@ -21,14 +21,17 @@ Random.seed!(100)
         x = randn(10)
         y = randn(10)
         for metric in (Euclidean(), TotalVariation())
-            pmetric = OptimalTransport.p2distance(metric, Val(p))
-            @test pmetric(x, y) ≈ metric(x, y)^p
+            for _p in (p, Val(p))
+                pmetric = OptimalTransport.p2distance(metric, _p)
+                @test pmetric(x, y) ≈ metric(x, y)^p
+            end
         end
     end
 
     @testset "prt" begin
         x = randexp()
         for p in (1, 2, 3, randexp())
+            @test OptimalTransport.prt(x, p) ≈ x^(1 / p)
             @test OptimalTransport.prt(x, Val(p)) ≈ x^(1 / p)
         end
     end
@@ -37,20 +40,22 @@ Random.seed!(100)
         μ = Normal(randn(), randexp())
         ν = Normal(randn(), randexp())
         for p in (1, 2, 3, randexp()), metric in (Euclidean(), TotalVariation())
-            # without additional keyword arguments
-            w = wasserstein(μ, ν; p=Val(p), metric=metric)
-            @test w ≈ ot_cost((x, y) -> metric(x, y)^p, μ, ν)^(1 / p)
+            for _p in (p, Val(p))
+                # without additional keyword arguments
+                w = wasserstein(μ, ν; p=_p, metric=metric)
+                @test w ≈ ot_cost((x, y) -> metric(x, y)^p, μ, ν)^(1 / p)
 
-            # with pre-computed plan (random `ν` ensures that plan is used)
-            T = ot_plan((x, y) -> metric(x, y)^p, μ, ν)
-            w2 = wasserstein(μ, Normal(randn(), rand()); p=Val(p), metric=metric, plan=T)
-            @test w ≈ w2
+                # with pre-computed plan (random `ν` ensures that plan is used)
+                T = ot_plan((x, y) -> metric(x, y)^p, μ, ν)
+                w2 = wasserstein(μ, Normal(randn(), rand()); p=_p, metric=metric, plan=T)
+                @test w ≈ w2
+            end
         end
 
         # check that `Euclidean` is the default `metric`
-        for p in (1, 2, 3, randexp())
-            w = wasserstein(μ, ν; p=Val(p))
-            @test w ≈ wasserstein(μ, ν; p=Val(p), metric=Euclidean())
+        for p in (1, 2, 3, randexp()), _p in (p, Val(p))
+            w = wasserstein(μ, ν; p=_p)
+            @test w ≈ wasserstein(μ, ν; p=_p, metric=Euclidean())
         end
 
         # check that `Val(1)` is the default `p`
