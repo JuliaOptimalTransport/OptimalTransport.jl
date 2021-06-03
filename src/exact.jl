96059ca272d2c7df00222e7f339cb5cb41839b75
Davi Sales Barreira <davisbarreira@gmail.com>
2021-06-03 10:58:12 -0300

    Update src/exact.jl
    Co-authored-by: David Widmann <devmotion@users.noreply.github.com>

--- src/exact.jl
+++ src/exact.jl
@@ -409,7 +409,7 @@ function ot_plan(::SqEuclidean, μ::MvNormal, ν::MvNormal)
 end
 
 """
-    ot_plan(c::SqEuclidean, μ::Normal, ν::Normal)
+    ot_plan(::SqEuclidean, μ::Normal, ν::Normal)
 
 Compute the optimal transport plan for the Monge-Kantorovich problem with
 normal distributions `μ` and `ν` as source and target marginals and cost function
