b335c1865e08a3a5a69aaa1634748bd85d03e5ad
Stephen Zhang <syz@math.ubc.ca>
2021-08-25 00:17:37 +1000

    don't specify size of identity

--- src/quadratic_newton.jl
+++ src/quadratic_newton.jl
@@ -118,7 +118,7 @@ function descent_dir!(solver::QuadraticOTSolver{<:QuadraticOTNewton})
     G[1:M, (M + 1):end] .= σ
     G[(M + 1):end, 1:M] .= σ'
     # G[diagind(G)] .+= δ # regularise cg
-    G .+= δ*(I(M + N))
+    G .+= δ*I
 
     # cg step
     b = -eps * vcat(vec(sum(γ; dims=2)) .- μ, vec(sum(γ; dims=1)) .- ν)
