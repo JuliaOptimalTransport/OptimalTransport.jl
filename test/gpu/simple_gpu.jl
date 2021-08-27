2a7667b66b6b5ce3ac4d591c9ce406c5b689ffc6
Stephen Zhang <syz@syz.id.au>
2021-08-27 04:41:33 +0000

    remove debug output

--- test/gpu/simple_gpu.jl
+++ test/gpu/simple_gpu.jl
@@ -96,7 +96,6 @@ Random.seed!(100)
 
         @testset "quadreg" begin
             # use a different reg parameter
-            ENV["JULIA_DEBUG"] = "OptimalTransport"
             ε_quad = 1.0f0
             γ = quadreg(cu_μ, cu_ν, cu_C, ε_quad, QuadraticOTNewton(0.1f0, 0.5f0, 1f-5, 50))
             # compare with results on the CPU
