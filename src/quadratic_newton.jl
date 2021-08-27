8a4dd1bfedf376b2e99b5cd09e9cb54820b87785
Stephen Zhang <syz@math.ubc.ca>
2021-08-27 13:48:54 +1000

    update docstrings

--- src/quadratic_newton.jl
+++ src/quadratic_newton.jl
@@ -19,6 +19,7 @@ end
     QuadraticOTNewton(;θ = 0.1, κ = 0.5, δ = 1e-5, armijo_max = 50)
 
 Semi-smooth Newton method (Algorithm 2 of Lorenz et al. 2019) with Armijo parameters `θ` and `κ`, and conjugate gradient regularisation `δ`. 
+`armijo_max` sets the maximum number of Armijo step size trials.
 
 See also: [`QuadraticOTNewton`](@ref), [`quadreg`](@ref)
 """
