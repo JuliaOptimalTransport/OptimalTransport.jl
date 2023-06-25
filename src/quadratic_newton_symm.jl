e757f7916ec10351f0aaf65c7d6b5ae46169fe20
Stephen Zhang <syz@syz.id.au>
2023-06-25 16:37:26 +1000

    draft implementation of active set method for QROT

--- src/quadratic_newton_symm.jl
+++ src/quadratic_newton_symm.jl
@@ -5,13 +5,25 @@ struct SymmetricQuadraticOTNewton{T<:Real,K<:Real,D<:Real} <: QuadraticOT
     armijo_max::Int
 end
 
-struct SymmetricQuadraticOTNewtonActiveSet{A<:SymmetricQuadraticOTNewton, ST<:AbstractSparseArray}
-    alg::A
+struct SymmetricQuadraticOTNewtonAS{T<:Real,K<:Real,D<:Real,ST<:AbstractSparseArray, IT, JT} <: QuadraticOT
+    θ::T
+    κ::K
+    δ::D
+    armijo_max::Int
     S::ST
+    I::IT
+    J::JT
 end
+
+Base.show(io::IO, ::SymmetricQuadraticOTNewtonAS) = print(io, "Symmetric semi-smooth Newton algorithm (active set)")
 
 function SymmetricQuadraticOTNewton(; θ=0.1, κ=0.5, δ=1e-5, armijo_max=50)
     return SymmetricQuadraticOTNewton(θ, κ, δ, armijo_max)
+end
+
+function SymmetricQuadraticOTNewtonAS(S; θ=0.1, κ=0.5, δ=1e-5, armijo_max=50)
+    I, J, V = findnz(S)
+    return SymmetricQuadraticOTNewtonAS(θ, κ, δ, armijo_max, S, I, J)
 end
 
 function check_convergence(
@@ -61,6 +73,45 @@ function descent_dir!(solver::QuadraticOTSolver{<:SymmetricQuadraticOTNewton})
     δu .= x
 end
 
+function descent_dir!(solver::QuadraticOTSolver{<:SymmetricQuadraticOTNewtonAS})
+    # unpack solver
+    eps = solver.eps
+    C = solver.C
+    μ = solver.source
+    cache = solver.cache
+    # unpack cache
+    u = cache.u
+    δu = cache.δu
+    σ = cache.σ
+    γ = cache.γ
+    G = cache.G
+    x = cache.x
+    M = cache.M
+    N = M
+    # Armijo parameters
+    δ = solver.alg.δ
+    S = solver.alg.S
+    I = solver.alg.I
+    J = solver.alg.J
+
+    # setup intermediate variables
+    @. γ.nzval = u[I] + u[J] - C.nzval
+    
+    @. σ.nzval = γ.nzval ≥ 0
+    @. γ.nzval = NNlib.relu(γ.nzval) / eps
+
+    # setup kernel matrix G
+    # G = Diagonal(vec(sum(σ; dims=2))) + σ + δ*I
+    fill!(G.nzval, 0)
+    G += σ
+    G += Diagonal(vec(sum(σ; dims=2)) .+ δ) 
+
+    # cg step
+    b = -eps * (vec(sum(γ; dims=2)) .- μ)
+    cg!(x, G, b)
+    δu .= x
+end
+
 function descent_step!(solver::QuadraticOTSolver{<:SymmetricQuadraticOTNewton})
     # unpack solver
     eps = solver.eps
@@ -94,6 +145,44 @@ function descent_step!(solver::QuadraticOTSolver{<:SymmetricQuadraticOTNewton})
     end
     return u .= u + t * δu
 end
+
+function descent_step!(solver::QuadraticOTSolver{<:SymmetricQuadraticOTNewtonAS})
+    # unpack solver
+    eps = solver.eps
+    C = solver.C
+    μ = solver.source
+    cache = solver.cache
+    # unpack cache
+    u = cache.u
+    δu = cache.δu
+    γ = cache.γ
+    S = solver.alg.S
+    I = solver.alg.I
+    J = solver.alg.J
+
+    # Armijo parameters
+    θ = solver.alg.θ
+    κ = solver.alg.κ
+    armijo_max = solver.alg.armijo_max
+    armijo_counter = 0
+
+    # dual objective 
+    function Φ(u, μ, C, ε, I, J)
+        return norm(NNlib.relu.(u[I] + u[J] - C.nzval))^2 / 2 - 2*ε * dot(μ, u) 
+    end
+
+    # compute directional derivative
+    d = -eps * (2*dot(δu, μ)) + eps * dot(γ.nzval, δu[I] + δu[J])
+    t = 1
+    Φ0 = Φ(u, μ, C, eps, I, J)
+    while (armijo_counter < armijo_max) &&
+        (Φ(u + t * δu, μ, C, eps, I, J) ≥ Φ0 + t * θ * d)
+        t = κ * t
+        armijo_counter += 1
+    end
+    return u .= u + t * δu
+end
+
 
 function solve!(solver::QuadraticOTSolver{<:SymmetricQuadraticOTNewton})
     # unpack solver
@@ -144,6 +233,68 @@ function solve!(solver::QuadraticOTSolver{<:SymmetricQuadraticOTNewton})
     return nothing
 end
 
+
+function solve!(solver::QuadraticOTSolver{<:SymmetricQuadraticOTNewtonAS})
+    # unpack solver
+    μ = solver.source
+    atol = solver.atol
+    rtol = solver.rtol
+    maxiter = solver.maxiter
+    check_convergence = solver.check_convergence
+    cache = solver.cache
+    convergence_cache = solver.convergence_cache
+    u_prev = similar(solver.cache.u)
+    copy!(u_prev, solver.cache.u)
+
+    function check_convergence_dual(u, u_prev, atol, rtol)
+        norm_diff = norm(u - u_prev)
+        isconverged =
+            norm_diff <
+            max(atol, rtol * max(norm(u), norm(u_prev)))
+        return isconverged, norm_diff
+    end
+    
+    isconverged = false
+    to_check_step = check_convergence
+    for iter in 1:maxiter
+        # compute descent direction
+        descent_dir!(solver)
+        # Newton step
+        descent_step!(solver)
+        # check source marginal
+        # always check convergence after the final iteration
+        to_check_step -= 1
+        if to_check_step == 0 || iter == maxiter
+            # reset counter
+            to_check_step = check_convergence
+
+            # isconverged, abserror = OptimalTransport.check_convergence(
+            #     μ, μ, cache, convergence_cache, atol, rtol
+            # )
+            isconverged, abserror = check_convergence_dual(solver.cache.u, u_prev, atol, rtol)
+            @debug string(solver.alg) *
+                   " (" *
+                   string(iter) *
+                   "/" *
+                   string(maxiter) *
+                   ": absolute error of source marginal = " *
+                   string(maximum(abserror))
+
+            if isconverged
+                @debug "$(solver.alg) ($iter/$maxiter): converged"
+                break
+            end
+            copy!(u_prev, solver.cache.u)
+        end
+    end
+
+    if !isconverged
+        @warn "$(solver.alg) ($maxiter/$maxiter): not converged"
+    end
+
+    return nothing
+end
+
 function build_cache(
     ::Type{T},
     ::SymmetricQuadraticOTNewton,
@@ -172,7 +323,33 @@ function build_cache(
     return QuadraticOTNewtonCache(u, v, δu, δv, σ, γ, G, x, M, N)
 end
 
-
+function build_cache(
+    ::Type{T}, 
+    alg::SymmetricQuadraticOTNewtonAS,
+    μ::AbstractVector, 
+    ν::AbstractVector, 
+    C::AbstractMatrix,
+    ε::Real
+    ) where {T}
+    # create and initialize dual potentials
+    u = similar(μ, T, size(μ, 1))
+    v = similar(ν, T, size(ν, 1))
+    fill!(u, zero(T))
+    fill!(v, zero(T))
+    δu = similar(u, T)
+    δv = similar(v, T)
+    # intermediate variables (don't need to be initialised)
+    σ = similar(C, T)
+    γ = similar(C, T)
+    M = size(μ, 1)
+    N = size(ν, 1)
+    # G = similar(u, T, M, M)
+    G = similar(alg.S + sparse(SparseArrays.I, M, M), T)
+    # initial guess for conjugate gradient 
+    x = similar(u, T, M)
+    fill!(x, zero(T))
+    return QuadraticOTNewtonCache(u, v, δu, δv, σ, γ, G, x, M, N)
+end 
 
 function build_solver(
     μ::AbstractVector,
@@ -216,9 +393,49 @@ function quadreg(μ, C, ε, alg::SymmetricQuadraticOTNewton; kwargs...)
     return γ
 end
 
+function quadreg(μ, C, ε, alg::SymmetricQuadraticOTNewtonAS; maxiter_as = 5, kwargs...)
+    function check_convergence(γ, cache, convergence_cache, atol, rtol)
+        norm_diff = norm(vec(sum(γ; dims=2)) .- μ, Inf)
+        isconverged =
+            norm_diff <
+            max(atol, rtol * max(convergence_cache.norm_source, convergence_cache.norm_target))
+        return isconverged, norm_diff
+    end
+    S = alg.S
+    I = alg.I
+    J = alg.J
+    γ = spzeros(size(C)...)
+    u = similar(μ)
+    fill!(u, 0)
+    for iter = 1:maxiter_as
+        _alg = SymmetricQuadraticOTNewtonAS(alg.θ, alg.κ, alg.δ, alg.armijo_max, S, I, J)
+        Csp_v = similar(C, length(_alg.I)); @inbounds for k = 1:length(_alg.I) Csp_v[k] = C[_alg.I[k], _alg.J[k]] end
+        solver = build_solver(μ, sparse(_alg.I, _alg.J, Csp_v), ε, _alg; kwargs...)
+        copy!(solver.cache.u, u)
+        solve!(solver)
+        copy!(u, solver.cache.u)
+        γ = plan(solver, C)
+        isconverged, norm_diff = check_convergence(γ, solver.cache, solver.convergence_cache, solver.atol, solver.rtol)
+        if isconverged
+            @debug "$(solver.alg) AS step ($iter/$maxiter_as): converged"
+            break
+        else
+            S = max.(_alg.S, sign.(γ))
+            I, J, _ = findnz(S)
+            @debug "$(solver.alg) AS growing support to $(nnz(S)) (sparsity $(nnz(S)/length(S)))"
+        end
+    end
+    return γ
+end
+
 function plan(solver::QuadraticOTSolver{<:SymmetricQuadraticOTNewton})
     cache = solver.cache
     γ = NNlib.relu.(cache.u .+ cache.u' .- solver.C) / solver.eps
     return γ
 end
 
+function plan(solver::QuadraticOTSolver{<:SymmetricQuadraticOTNewtonAS}, Cfull)
+    cache = solver.cache
+    γ = sparse(NNlib.relu.(cache.u .+ cache.u' .- Cfull) / solver.eps)
+    return γ
+end
--- test.jl
+++ test.jl
@@ -6,22 +6,28 @@ using LinearAlgebra
 using SparseArrays
 using Distances
 using StatsBase
+using NNlib
+using LazyArrays
+using IterativeSolvers
 
-N = 500
-d = 500
+N = 10_000
+d = 250
 μ_spt = randn(N, d)
 X = μ_spt
 μ = ones(N)
 C = sum(X.^2 ; dims = 2)/2 .+ sum(X.^2 ; dims = 2)'/2 - X * X'
+Cmean = mean(C)
 C[diagind(C)] .= Inf
+ENV["JULIA_DEBUG"] = "OptimalTransport"
 
-ε = 5.0
-π = sparse(quadreg(μ, C, ε, OptimalTransport.SymmetricQuadraticOTNewton(); maxiter = 25))
+ε = 1.0
+π = sparse(quadreg(μ, C / Cmean, ε, OptimalTransport.SymmetricQuadraticOTNewton(); maxiter = 25))
 
-using RandomMatrix
+nnz(π) / N^2
 
-# KNN initialisation
+using NearestNeighborDescent
 using NearestNeighbors
+
 function knn_adj(X, k)
     # indices, _ = knn_matrices(nndescent(X, k, Euclidean())); 
     indices, _ = knn(KDTree(X), X, k);
@@ -35,56 +41,87 @@ function knn_adj(X, k)
     return A
 end
 
-S = knn_adj(X', 25)
-S += sign.(mean([randPermutation(N) for _ in 1:25]))
-S = sign.(S)
-S[diagind(S)] .= 0
-dropzeros!(S)
+S = knn_adj(X', 50);
+S += sign.(mean([randPermutation(N) for _ in 1:50])); 
+S = sign.(S); 
+S[diagind(S)] .= 0;
+dropzeros!(S);
 
-using NNlib
-using LazyArrays
-using IterativeSolvers
+nnz(S) / N^2
+
+ε = 1
+alg = OptimalTransport.SymmetricQuadraticOTNewtonAS(S)
+γ = quadreg(μ, C / Cmean, ε, alg; maxiter = 100)
+
+sum(γ; dims = 2)
+sum(γ; dims = 1)
+
+diag(γ)
+
+norm(π - γ, 1)
 
 D = sum(X.^2 ; dims = 2)/2
+alg = OptimalTransport.SymmetricQuadraticOTNewtonAS(S)
+# form partial cost matrix
+I, J, V = findnz(S)
+C = D[I] + D[J]; @time @inbounds for k = 1:length(V) C[k] += -dot(X[I[k], :], X[J[k], :]) end
+C_sp = sparse(I, J, relu.(C))
+solver = OptimalTransport.build_solver(μ, C_sp, ε, alg; maxiter = 50)
+OptimalTransport.solve!(solver);
+# form full cost matrix
+Cfull = sum(X.^2 ; dims = 2)/2 .+ sum(X.^2 ; dims = 2)'/2 - X * X';
+Cfull[diagind(Cfull)] .= Inf;
+plan = OptimalTransport.plan(solver, Cfull)
+S = max.(S, sign.(plan))
 
+norm(sum(plan; dims = 1) .- 1)
+norm(sum(plan; dims = 2) .- 1)
+
+nnz(plan) / N^2
+
+plot(heatmap(Array(plan)), heatmap(Array(π)))
+
+"""
+D = sum(X.^2 ; dims = 2)/2
+Csp = D[I] + D[J]; @time @inbounds for k = 1:length(V) Csp[k] += -dot(X[I[k], :], X[J[k], :]) end
+C = sum(X.^2 ; dims = 2)/2 .+ sum(X.^2 ; dims = 2)'/2 - X * X'
+C[diagind(C)] .= Inf
 ## start 
 u = zeros(N)
 
-function Φ(u, μ, C, ε, S)
-    return norm(NNlib.relu.((u .* S) .+ (u' .* S) .- C))^2 / 2 - 2*ε * dot(μ, u) 
+function Φ(u, μ, C, ε, I, J)
+    return norm(NNlib.relu.(u[I] + u[J] - C))^2 / 2 - 2*ε * dot(μ, u) 
 end
 
 armijo_max = 25
+δ = 1e-5
 for it = 1:10
     @info it
-    Csp = similar(S);
-    I, J, V = findnz(Csp);
-    for (i, j) in zip(I, J) Csp[i, j] = D[i] + D[j] - dot(X[i, :], X[j, :]) end
-
-    γ = (u .* S) + (u' .* S) - Csp
-    σ = similar(S)
-    I, J, V = findnz(σ)
-    for (i, j) in zip(I, J) σ[i, j] = (γ[i, j] ≥ 0) end
-    dropzeros!(σ)
-    γ = relu.(γ) / ε
-    δ = 1e-5
-
-    G = σ + Diagonal(vec(sum(σ; dims = 2)) .+ δ)
-    b = -ε*(vec(sum(γ; dims = 2)) .- 1)
+    I, J, V = findnz(S)
+    Csp = similar(X, length(V));
+    @inbounds for k = 1:length(V) Csp[k] = D[I[k]] + D[J[k]] - dot(X[I[k], :], X[J[k], :]) end
+    γ = u[I] + u[J] - Csp
+    σ = similar(Csp)
+    @. σ = γ ≥ 0
+    @. γ = relu(γ) / ε
+    σ_sp = sparse(I, J, σ)
+    γ_sp = sparse(I, J, γ)
+    G = σ_sp + Diagonal(vec(sum(σ_sp; dims = 2)) .+ δ)
+    b = -ε*(vec(sum(γ_sp; dims = 2)) .- 1)
 
     δu = similar(u)
     fill!(δu, 0)
     cg!(δu, G, b)
 
-
-    d = ε*sum(γ .* ((δu .* S) + (δu' .* S))) - 2ε*dot(δu, μ)
+    d = ε*dot(γ, δu[I] + δu[J]) - 2ε*dot(δu, μ)
+    @info d
     t = 1
-    Φ0 = Φ(u, μ, Csp, ε, S)
+    Φ0 = Φ(u, μ, Csp, ε, I, J)
     armijo_counter = 0
-    θ = alg.alg.θ
-    κ = alg.alg.κ
+    θ = alg.θ
+    κ = alg.κ
     while (armijo_counter < armijo_max) &&
-        (Φ(u + t * δu, μ, Csp, ε, S) ≥ Φ0 + t * θ * d)
+        (Φ(u + t * δu, μ, Csp, ε, I, J) ≥ Φ0 + t * θ * d)
         t = κ * t
         armijo_counter += 1
     end
@@ -100,6 +137,6 @@ sum(γ; dims = 2)
 sparse(γ)
 
 
-
 using Plots
 plot(heatmap(Array(γ)), heatmap(Array(π)))
+"""
